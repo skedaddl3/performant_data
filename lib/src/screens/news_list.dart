fbbf763b7bbe64f8ccefb614be7f96c4424b2363
skedaddl3 <hexisdylan23@gmail.com>
2022-04-28 14:04:31 +0800

    change m

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:performant_data/src/blocs/stories_bloc.dart';
+import '../blocs/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   Widget build(context) {
+    final bloc = StoriesProvider.of(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: buildList(),
+      body: buildList(bloc),
     );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+        stream: bloc.topIds,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Text('Still waiting on Ids');
+          }
+
+          return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, int index) {
+                return Text(snapshot.data[index]);
+              });
+        });
   }
 
   Widget buildList() {
